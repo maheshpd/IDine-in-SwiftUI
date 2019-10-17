b135caac64d28213c250180593a1440f43315a9b
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-10-17 22:30:58 +0530

    Initial Commit

---
+++ IDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IDine
+//
+//  Created by Mahesh Prasad on 17/10/19.
+//  Copyright © 2019 Create Apps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
